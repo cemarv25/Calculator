5d8bcfb19de4ec7df43931f2a7cff18bf841841e
user188731 <user188731@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2021-02-28 18:00:27 -0800

    Added operation buttons functionality, % and () pending

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,12 +8,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var current = "0"
+    var operation = ""
+    var delResultText = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func calculate(x: Double, y: Double, operation: String) -> Double {
+        switch operation {
+        case "+":
+            return x + y
+        case "-":
+            return x - y
+        case "×":
+            return x * y
+        case "÷":
+            return x / y
+        default:
+            return y
+        }
+    }
 
-
+    @IBAction func processNumber(_ sender: UIButton) {
+            if let numberText = sender.titleLabel?.text {
+                if !delResultText {
+                    if (resultLabel.text == "0") {
+                        resultLabel.text = numberText
+                    } else {
+                        resultLabel.text = resultLabel.text! + numberText
+                    }
+                } else {
+                    resultLabel.text = numberText
+                    delResultText = false
+                }
+                
+            }
+        }
+    
+    @IBAction func processEquals(_ sender: UIButton) {
+        let x = Double(current)
+        let y = Double(resultLabel.text!)
+        let result = calculate(x: x!, y: y!, operation: operation)
+        resultLabel.text = String(result)
+        current = "0"
+        delResultText = true
+        operation = ""
+    }
+    
+    
+    @IBAction func processClear(_ sender: UIButton) {
+        resultLabel.text = "0"
+        current = "0"
+        delResultText = true
+        operation = ""
+    }
+    
+    @IBAction func processOperation(_ sender: UIButton) {
+        if let operationText = sender.titleLabel?.text {
+            if operation.isEmpty {
+                operation = operationText
+                current = resultLabel.text ?? "0"
+                delResultText = true
+                
+            } else {
+                let x = Double(current)
+                let y = Double(resultLabel.text!)
+                let result = calculate(x: x!, y: y!, operation: operation)
+                current = String(result)
+                resultLabel.text = String(result)
+                operation = operationText
+                delResultText = true
+            }
+        }
+    }
+    
+    
 }
 
